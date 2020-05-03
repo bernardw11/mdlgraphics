1655c7a185d5ba55342b8109979bad664fa1eabc
bernardw11 <bwang00@stuy.edu>
2020-05-03 15:40:21 -0400

    jesus christ

--- script.py
+++ script.py
@@ -21,8 +21,8 @@ def run(filename):
     ambient = [50,
                50,
                50]
-    light = [[0.5,
-              0.75,
+    light = [[0.5, #0.5
+              .75, #0.75
               1],
              [255,
               255,
@@ -49,3 +49,66 @@ def run(filename):
     print(symbols)
     for command in commands:
         print(command)
+        #sphere, torus, box
+        if command['op'] == 'sphere' or command['op'] == 'torus' or command['op'] == 'box':
+            if command['op'] == 'sphere':
+                add_sphere(tmp,
+                           float(command['args'][0]), float(command['args'][1]),
+                           float(command['args'][2]), float(command['args'][3]), step_3d)
+            elif command['op'] == 'torus':
+                add_torus(tmp,
+                           float(command['args'][0]), float(command['args'][1]),
+                           float(command['args'][2]), float(command['args'][3]),
+                           float(command['args'][4]), step_3d)
+            elif command ['op'] == 'box':
+                add_box(tmp,
+                           float(command['args'][0]), float(command['args'][1]),
+                           float(command['args'][2]), float(command['args'][3]),
+                           float(command['args'][4]), float(command['args'][5]))
+            matrix_mult(stack[-1], tmp)
+            if command['constants']:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+            else:
+                draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
+            tmp = []
+
+        #constants
+        elif command['op'] == 'constants':
+            pass
+        elif command['op'] == 'line':
+            add_edge(tmp,
+                      float(command['args'][0]), float(command['args'][1]),
+                      float(command['args'][2]), float(command['args'][3]),
+                      float(command['args'][4]), float(command['args'][5]))
+            matrix_mult(stack[-1], edges)
+            draw_lines(edges, screen, zbuffer, color)
+            tmp = []
+
+        #move, rotate, scale
+        elif command['op'] == 'move' or command['op'] == 'rotate' or command['op'] == 'scale':
+            if command['op'] == 'move':
+                t = make_translate(float(command['args'][0]), float(command['args'][1]),
+                                   float(command['args'][2]))
+            elif command['op'] == 'rotate':
+                theta = float(command['args'][1]) * (math.pi / 180)
+                if command['args'][0] == 'x':
+                    t = make_rotX(theta)
+                elif command['args'][0] == 'y':
+                    t = make_rotY(theta)
+                else:
+                    t = make_rotZ(theta)
+            elif command['op'] == 'scale':
+                t = make_scale(command['args'][0], command['args'][1], command['args'][2])
+            matrix_mult(stack[-1], t)
+            stack[-1] = [ x[:] for x in t]
+
+        elif command['op'] == 'push':
+            stack.append([x[:] for x in stack[-1]])
+        elif command['op'] == 'pop':
+            stack.pop()
+        elif command['op'] == 'save':
+            save_extension(screen, command['args'][0] + ".ppm")
+        elif command['op'] == 'display':
+            display(screen)
+        else:
+            print("done goofed")
